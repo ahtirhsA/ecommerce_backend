c61c520b4b6e564e0772dd15d3315cf994377098
ashritha <ashrithavollala2002@gmail.com>
2025-03-12 16:03:25 +0530

    sol

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const upload = multer({ dest: './uploads/' });
 
 
 const cors=require('cors')
-app.use(cors())
+app.use(cors(
+    {
+        origin:"*"
+    }
+))
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
